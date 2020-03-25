076476bf0c17f70384a994534228d2d1ab38270a
Herbert Feliciano Beserra <Herbert.beserra@gmail.com>
2020-03-25 20:59:44 -0300

    Cors erro

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = require('./routes')
+var cors = require('cors')  
 
 const app = express()
 
@@ -11,25 +12,3 @@ app.use(express.json())
 app.use(routes)
 
 app.listen(3333)
-
-
-/*app.post('/users/:id', (req,res)=>{
-    
-    const QueryParams = req.query // a variavel QueryParams recebe os parametros enviados na requisicão pelo metodo Query
-
-    console.log(QueryParams)
-    
-    const RouteParms = req.params // a variavel RouteParams recebe os parametros enviados na requisicão pelo metodo Route
-
-    console.log(RouteParms)
-
-    const RequestBody = req.body // a variavel RequestBody recebe o corpo da requisição
-
-    console.log(RequestBody)
-
-    return res.json({
-        evento: 'Semana OmniStack 11.0',
-        aluno: 'HBeserra'
-    })
-
-})*/
